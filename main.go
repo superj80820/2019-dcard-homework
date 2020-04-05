7b87ef452b686863d9eb8f3ecc388c235405004e
superj80820 <superj80820@gmail.com>
2020-04-06 03:00:58 +0800

    fix: Fix race condition error

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	router := gin.Default()
 
 	dcard := router.Group("/dcard")
+	dcard.Use(middleware.MaxAllowed(1)) //限制同時連線的數量，以避免mongoDB的limit collection產生race condition。TODO: 對特定IP限制同時連線的數量，而不是全部的IP，這樣才不會對效能產生影響，應該可用redis實作
 	dcard.Use(middleware.RateLimit())
 	// dcard.Use(ACL.MiddlewareRateLimit()) TODO: should implement
 	{
